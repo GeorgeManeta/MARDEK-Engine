7a51876340bc540d2da37d10ed250bcb059c8215
link3710 <leosmund@yahoo.com>
2022-09-10 01:12:37 -0400

    Testing Serialization Instead
    Tried Serializing, fields aren't serializing for some reason...

--- MARDEK Engine/Assets/Scripts/MARDEK/Inventory/Item/JSONReader.cs
+++ MARDEK Engine/Assets/Scripts/MARDEK/Inventory/Item/JSONReader.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using FullSerializer;
+using MARDEK.Core;
+using MARDEK.Stats;
+using MARDEK.Skill;
 
 namespace MARDEK.Inventory
 {
@@ -9,10 +12,30 @@ namespace MARDEK.Inventory
 
         void Start()
         {
-            Debug.Log(jsonFile.text);
-            fsData data = fsJsonParser.Parse(jsonFile.text);
+            //Debug.Log(jsonFile.text);
+            //fsData data = fsJsonParser.Parse(jsonFile.text);
+            //Debug.Log(data);
+            UnobtainedPlotItems items = new UnobtainedPlotItems();
+            items.items = new UnobtainedPlotItem[1];
+            items.items[0] = ScriptableObject.CreateInstance<UnobtainedPlotItem>();
+            UnobtainedPlotItem test = items.items[0];
+            test.displayName = "test name";
+            test.description = "test descript";
+            test.element = Element.CreateInstance<Element>();
+            test.element.name = "Fire";
+            test.price = -1;
+            items.items[0] = test;
+            Debug.Log(test);
+            Debug.Log(test.displayName);
+            Debug.Log(test.element);
+            fsData data;
 
-            UnobtainedPlotItems items = null;
+            fsSerializer serializer = new fsSerializer();
+            serializer.TrySerialize(items, out data).AssertSuccessWithoutWarnings();
+
+            Debug.Log(data);            
+            
+            /*UnobtainedPlotItems items = null;
             fsSerializer serializer = new fsSerializer();
             serializer.TryDeserialize(data, ref items).AssertSuccessWithoutWarnings();
             Debug.Log("items");
@@ -23,7 +46,7 @@ namespace MARDEK.Inventory
             foreach (UnobtainedPlotItem item in items.items)
             {
                 Debug.Log("Found item: " + item.displayName);
-            }
+            }*/
         }
     }
 }
