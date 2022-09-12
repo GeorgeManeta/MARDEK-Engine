a445c987ef1dfbe7c5353f22f2f496ff9bc39368
link3710 <leosmund@yahoo.com>
2022-09-11 21:12:29 -0400

    Shield Sprites
    Added sprites for all shields

---
+++ MARDEK Engine/Assets/Scripts/MARDEK/Inventory/Item/PlotItem.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace MARDEK.Inventory
+{
+    /*
+     * Represents a `PlotItem`. Such items can be stored in a shop inventory and a battle loot
+     * inventory.
+     */
+    public class PlotItem : Item
+    {
+        
+    }
+}
