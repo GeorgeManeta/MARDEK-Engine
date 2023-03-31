b86887962eb9dcefad60fa3af685158a76596261
GeorgeManeta <georgemaneta@gmail.com>
2023-03-31 13:38:39 -0300

    Fix movement bug

--- MARDEK Engine/Assets/Scripts/Input/PlayerControls.cs
+++ MARDEK Engine/Assets/Scripts/Input/PlayerControls.cs
@@ -29,7 +29,7 @@ public partial class @PlayerControls : IInputActionCollection2, IDisposable
             ""actions"": [
                 {
                     ""name"": ""Movement"",
-                    ""type"": ""Button"",
+                    ""type"": ""PassThrough"",
                     ""id"": ""6c609b02-eeaf-4e17-ac5a-aeb8ed3508b8"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
