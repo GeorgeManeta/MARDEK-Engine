eed1f92d19eceb0a84b078b76b79018493d92bdf
GeorgeManeta <georgemaneta@gmail.com>
2024-06-23 12:43:28 -0300

    Make the skill scriptable objects addressables

--- MARDEK Engine/Assets/Scripts/MARDEK/Skill/Skills/Skill.cs
+++ MARDEK Engine/Assets/Scripts/MARDEK/Skill/Skills/Skill.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MARDEK.Stats;
+using MARDEK.Core;
 
 namespace MARDEK.Skill
 {
-    public abstract class Skill : ScriptableObject
+    public abstract class Skill : AddressableScriptableObject
     {
         [field: SerializeField] public string DisplayName { get; private set; }
         [field: SerializeField] public string Description { get; private set; }
